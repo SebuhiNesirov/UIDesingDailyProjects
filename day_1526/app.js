3a08fcc70f90e5606b8cd8ab3f88205c9c09173f
SebuhiNesirov <sebuhi.nesirov.95@gmail.com>
2023-03-11 21:01:16 +0400

    Active Class
    Update active class

--- day_1526/app.js
+++ day_1526/app.js
@@ -1,3 +1,9 @@
 const link = document.querySelector(".link");
 const items = document.querySelectorAll(".items");
 
+items.forEach(function(div){
+    div.addEventListener("click", function(){
+        link.querySelector(".active").classList.remove("active");
+        div.classList.add("active");
+    })
+})
